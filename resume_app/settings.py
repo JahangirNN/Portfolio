ef5448a7d163f2e81ef3d762302baa1f69446b53
Jahangir Ashraf Nasirwala <acerdesktop53@gmail.com>
2023-01-17 03:59:25 +0530

    Deployment

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercal.app', '.now.sh']
 
 
 # Application definition
@@ -139,3 +139,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build' ,'static')
