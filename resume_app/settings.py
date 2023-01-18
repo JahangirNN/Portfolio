b69c801ddc304cbfda5630f8a2a3850cc0b15729
Jahangir Ashraf Nasirwala <acerdesktop53@gmail.com>
2023-01-18 17:56:07 +0530

    minor change

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
