9b4257df107d37df0a9a868a26cb48336d09f795
Jahangir Ashraf Nasirwala <acerdesktop53@gmail.com>
2023-02-23 03:38:56 +0530

    minor change

--- main/views.py
+++ main/views.py
@@ -25,7 +25,21 @@ class IndexView(generic.TemplateView):
 		certificates = Certificate.objects.filter(is_active=True)
 		blogs = Blog.objects.filter(is_active=True)
 		portfolio = Portfolio.objects.all()
-		skill = Skill.objects.filter()
+		skill = []
+		s_1 = Skill(name = "Python", is_key_skill = True)
+		s_2 = Skill(name = "Python-FastAPI", is_key_skill = True)
+		s_3 = Skill(name = "Python-Django", is_key_skill = True)
+		s_4 = Skill(name = "React Js", is_key_skill = True)
+		s_5 = Skill(name = "PostgreSQL", is_key_skill = True)
+		s_6 = Skill(name = "MongoDb", is_key_skill = True)
+		s_1.save()
+		s_2.save()
+		s_3.save()
+		s_4.save()
+		s_5.save()
+		s_6.save()
+
+		skill.append(s_1)
 		context["testimonials"] = testimonials
 		context["certificates"] = certificates
 		context["blogs"] = blogs
